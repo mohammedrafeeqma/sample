4c2319f78ff5dcbe6a496ca702669d006be0fd58
mohammedrafeeqma <mohammedrafeeqma@gmail.com>
2022-04-13 17:48:56 +0530

    bug fixes

--- backend/app.js
+++ backend/app.js
@@ -42,9 +42,9 @@ app.use('/api/notification',notificationRoute)
 
 
 
-app.use(express.static(path.join(__dirname,'../', '/frontend/build')))
+app.use(express.static(path.join(__dirname,'../../', '/client/deploy')))
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname,'../', 'frontend', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname,'../../', 'client', 'deploy', 'index.html'))
   )
 
 
